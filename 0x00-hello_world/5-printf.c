ed4be8a709aec2ede629e4bc86b59069fa2c1b8c
Eliez-alx <hailueliezer@gmail.com>
2023-02-16 11:53:35 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program to print a line by printf function
+ * Return: 0
+ */
+
+int main(void)
+{
+ptintf("with proper grammar, but the outcome is a piece of art, \n")
+return (0);
+}
