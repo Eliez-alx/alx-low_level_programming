ec20e147b48a139de0b911954b0151967be687c2
Eliez-alx <hailueliezer@gmail.com>
2023-02-22 11:50:21 -0800

    11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,11 +15,11 @@ void print_to_98(int n)
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-				ptintf(" % d, ", i);
+				printf(" % d, ", i);
 			else if (i == 98)
-				ptintf(" % d\n", i);
+				printf(" % d\n", i);
 		}
-	}else if (n >= 98)
+	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
@@ -30,4 +30,3 @@ void print_to_98(int n)
 		}
 	}
 }
-
