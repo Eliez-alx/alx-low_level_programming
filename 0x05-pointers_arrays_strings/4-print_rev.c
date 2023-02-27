68eb0d2fa82b8efd105c7b081d9a4452acef1f44
Eliez-alx <hailueliezer@gmail.com>
2023-02-27 08:36:29 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: pointer to be changed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+}
