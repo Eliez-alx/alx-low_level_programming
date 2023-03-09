821367f1f025441c748e085af0bbff827f7b8c50
Eliez-alx <hailueliezer@gmail.com>
2023-03-08 17:28:18 -0800

    101-wildcmp.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion.c - returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: number to be multiplied
  * @y: number to multiply x
  * Return: value multiplied by y
