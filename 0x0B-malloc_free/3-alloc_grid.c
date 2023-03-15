87b086a73d1a0d1164360edd234b7335781d3024
Eliez-alx <hailueliezer@gmail.com>
2023-03-15 07:12:30 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
 /**
  * **alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: input
- * #height: input
+ * @height: input
  * Return: pointer to 2 dimensional array
  */
 int **alloc_grid(int width, int height)
