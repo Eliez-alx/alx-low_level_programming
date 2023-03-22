d0269bdf49934083b4f48687fc8168f1cd41da23
Eliez-alx <hailueliezer@gmail.com>
2023-03-22 13:02:48 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -4,6 +4,7 @@
  * main - Prints its own opcodes main function
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
@@ -18,11 +19,13 @@ int main(int argc, char *argv[])
 	}
 
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
+
 	arr = (char *)main;
 
 	for (i = 0; i < bytes; i++)
@@ -32,7 +35,7 @@ int main(int argc, char *argv[])
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
