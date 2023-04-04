7b32d160af18d38ddc3dabdc01d3e2466e07b2cd
Eliz-alx <hailueliezer@gmail.com>
2023-04-04 12:15:45 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * listint_len - return number of elements in link
+ * @h: linked list to traverse
+ * Return: integers
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while(h);
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
