b212eeec988deddfd3d78842a810770f59103361
Eliz-alx <hailueliezer@gmail.com>
2023-04-04 10:11:02 -0700

    2-add_nodeint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,9 +7,9 @@
  * Return: integer
  */
 size_t print_listint(const listint_t *h)
-{       
-        const listint_t *tp;
-        unsigned int cntr = 0;
+{
+	const listint_t *tp;
+	unsigned int cntr = 0;
 
 	tp = h;
 	while (tp)
